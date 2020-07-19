27347bfacc4b7f42745165000ec998d05a62d15a
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-19 16:39:45 +0530

    Header, Footer and Note added

---
+++ notes-app/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Footer() {
+  var currentYear = new Date().getFullYear();
+  return <footer><p>Copyright &copy {currentYear}</p></footer>;
+}
+
+export default Footer;
--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Notes App</h1>,
+  <App />,
   document.getElementById('root')
 );
