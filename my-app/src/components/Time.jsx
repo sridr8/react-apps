62b5c4038e4a4dfd9cbdd17ac9b35704fe871235
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-20 12:52:24 +0530

    useState hook to change button color

--- my-app/src/components/Time.jsx
+++ my-app/src/components/Time.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Time(props) {
+  const [isMouseOver, setMouseOver] = useState(false);
+function handleMouseOver() {
+  setMouseOver(true);
+}
+function handleMouseOut() {
+  setMouseOver(false);
+}
   return (
     <div className="container">
       <h1 className="label">{props.time}</h1>
-      <button onClick={props.update}>Get Time</button>
+      <button 
+      style={{backgroundColor: isMouseOver ? "#2d3436":"#1a7e76"}}
+      onClick={props.update}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}>Get Time</button>
     </div>
   );
 }
