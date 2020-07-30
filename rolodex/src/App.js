b6fd289ee6e609731fbfdff2eed1ee63159290b1
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-30 13:24:28 +0530

    class component

--- rolodex/src/App.js
+++ rolodex/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sri Saibaba Sarath Babuji
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Jyothi Saisuhas
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+    this.state = { 
+      name : 'Jyothi', 
+      occupation : 'Maths Scientist'
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            React App 
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jyos
+          </a>
+          <br/>
+          <button onClick= {() => this.setState({ occupation: 'Also an excellent cook!'})}>Click me</button>
+          <p>{this.state.occupation}</p>
+        </header>
+      </div>
+    );
+    }
 }
 
 export default App;
