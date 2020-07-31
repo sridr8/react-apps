88ffcfb7794d1b0822cd843751ebb68b132b20dd
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-31 11:26:48 +0530

    Machine component

--- slot-machines/src/App.js
+++ slot-machines/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Machine from "./components/Machine";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Slot Machines!</h1>
+        <Machine />
       </div>
     );
   }
---
+++ slot-machines/src/components/Machine.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Machine extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <p>A</p>
+          <p>A</p>
+          <p>A</p>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Machine;
