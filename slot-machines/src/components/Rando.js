de5f274df54b9794ba0769d7d155fb7ae4e702a9
Sridhar Kothapalli <sridr8@gmail.com>
2020-08-01 15:16:08 +0530

    state applied onClick event of Button

--- slot-machines/src/components/Rando.js
+++ slot-machines/src/components/Rando.js
@@ -4,8 +4,9 @@ class Rando extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      num: 7,
+      num: 0,
     };
+    this.updateNumber = this.updateNumber.bind(this);
     //this.makeTimer(); // uncomment to run the timer function
   }
   makeTimer() {
@@ -16,10 +17,22 @@ class Rando extends Component {
       });
     }, 1000);
   }
+
+  updateNumber() {
+    let randNum = Math.floor(Math.random() * this.props.maxNum) + 1;
+    this.setState({
+      num: randNum,
+    });
+  }
   render() {
     return (
       <div>
-        <h3>{this.state.num}</h3>
+        <h3>Number is: {this.state.num}</h3>
+        {this.state.num === 7 ? (
+          "WINNER!"
+        ) : (
+          <button onClick={this.updateNumber}>Click Me!</button>
+        )}
       </div>
     );
   }
