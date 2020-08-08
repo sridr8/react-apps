b7e01803c2af6f423ad553f00c025105757411fd
Sridhar Kothapalli <sridr8@gmail.com>
2020-08-08 20:49:10 +0530

    cleanup

--- picsearch/src/index.js
+++ picsearch/src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+class App extends React.Component {
+  render() {
+    return <div>Pics Search!</div>;
+  }
+}
 ReactDOM.render(<App />, document.getElementById("root"));
