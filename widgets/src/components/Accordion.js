44d25c2afe8f905558822f50b57ee494c8f47eb9
Sridhar Kothapalli <sridr8@gmail.com>
2020-09-08 19:24:17 +0530

    accordion feature implemented

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment>
-        <div className="title active">
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon" />
           {item.title}
         </div>
-        <div className="content active">{item.content}</div>
+        <div className={`content ${active}`}>{item.content}</div>
       </React.Fragment>
     );
   });
